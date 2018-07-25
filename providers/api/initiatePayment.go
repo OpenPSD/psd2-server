208cd44231301d787fae4704be8a4ad7a56031b4
Oliver Lauer <oliverlauer@Olivers-MacBook-Pro.local>
2018-07-25 20:07:53 +0200

    Sepa CT Response

--- providers/api/initiatePayment.go
+++ providers/api/initiatePayment.go
@@ -9,5 +9,69 @@ import (
 
 // InitiatePayment returns an payment by ID
 func (s Psd2HttpServer) InitiatePayment(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "InitiatePayment with method: %s \n", ps.ByName("payment-product"))
+	//fmt.Fprintf(w, "InitiatePayment with method: %s \n", ps.ByName("payment-product"))
+	ctResponse := `{
+		"transactionStatus": "ACTC",
+		"paymentId": "0007.01",
+		"transactionFees": {
+		  "currency": "EUR",
+		  "amount": "0.42"
+		},
+		"transactionFeeIndicator": true,
+		"scaMethods": [
+		  {
+			"authenticationType": "SMS_OTP",
+			"authenticationVersion": "string",
+			"authenticationMethodId": "string",
+			"name": "string",
+			"explanation": "string"
+		  }
+		],
+		"chosenScaMethod": {
+		  "authenticationType": "SMS_OTP",
+		  "authenticationVersion": "string",
+		  "authenticationMethodId": "string",
+		  "name": "string",
+		  "explanation": "string"
+		},
+		"challengeData": {
+		  "image": "string",
+		  "data": "string",
+		  "imageLink": "string",
+		  "otpMaxLength": 0,
+		  "otpFormat": "characters",
+		  "additionalInformation": "string"
+		},
+		"_links": {
+		  "scaRedirect": "string",
+		  "scaOAuth": "string",
+		  "updatePsuIdentification": "string",
+		  "updateProprietaryData": "string",
+		  "updatePsuAuthentication": "string",
+		  "selectAuthenticationMethod": "string",
+		  "authoriseTransaction": "string",
+		  "self": "string",
+		  "status": "string",
+		  "account": "string",
+		  "balances": "string",
+		  "transactions": "string",
+		  "transactionsDetails": "string",
+		  "first": "string",
+		  "next": "string",
+		  "previous": "string",
+		  "last": "string",
+		  "download": "string"
+		},
+		"psuMessage": "string",
+		"tppMessages": [
+		  {
+			"category": "ERROR",
+			"code": "TOKEN_INVALID",
+			"text": "additional text information of the ASPSP up to 512 characters",
+			"path": "string"
+		  }
+		]
+	  }`
+	fmt.Fprintf(w, ctResponse)
+
 }
