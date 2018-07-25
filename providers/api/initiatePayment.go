ec88663985c2b2416e96766512933a4c5848a7ab
Oliver Lauer <oliverlauer@Olivers-MacBook-Pro.local>
2018-07-25 15:38:39 +0200

    sepa-credit-transfer

--- providers/api/initiatePayment.go
+++ providers/api/initiatePayment.go
@@ -9,5 +9,5 @@ import (
 
 // InitiatePayment returns an payment by ID
 func (s Psd2HttpServer) InitiatePayment(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "InitiatePayment with method, %s!\n", ps.ByName("payment-method"))
+	fmt.Fprintf(w, "InitiatePayment with method: %s \n", ps.ByName("payment-product"))
 }
