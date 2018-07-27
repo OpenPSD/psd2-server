e299ecf2ba2858e4f631b1219cc4f86de8d85a71
Oliver Lauer <oliverlauer@Olivers-MacBook-Pro.local>
2018-07-27 17:01:00 +0200

    Response for embedded for authorising of authorisation resource included

--- providers/api/authorisePayment.go
+++ providers/api/authorisePayment.go
@@ -1,21 +1,37 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
 
-// AuthorisePayment authorises a payment by ID
+// AuthorisePayment authorises a payment by payment- and authorisation ID
 func (s Psd2HttpServer) AuthorisePayment(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Println("Authorise payment with payment ID:", ps.ByName("paymentid"))
 	fmt.Println("Authorise payment with authorisation ID:", ps.ByName("authorisationid"))
 
+	type AuthorisePayment struct {
+		PsuData struct {
+			Password string `json:"password"`
+		} `json:"psuData"`
+	}
+
+	var authorisePayment AuthorisePayment
+
+	err := json.NewDecoder(r.Body).Decode(&authorisePayment)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	fmt.Println("Authorise Request Body: ", authorisePayment)
+
 	authoriseResponse := `{
 		"scaStatus": "psuAuthenticated",
 	  "_links":{
-	  "authoriseTransaction": {"href": "/v1/payments/axa-pay-paymentid-1234/authorisations/123axa-auth456"}
+	  "authoriseTransaction": {"href": "/payments/axa-pay-paymentid-1234/authorisations/123axa-auth456"}
 		}
 	  }`
 	fmt.Fprintf(w, authoriseResponse)
